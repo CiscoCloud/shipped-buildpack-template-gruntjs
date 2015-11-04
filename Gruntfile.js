352d733e453f075f2e3ec2290da197e3d994d749
Brendan McElligott <brendan.mcelligott@gmail.com>
2015-11-04 14:46:45 -0700

    Make grunt work better as default task

--- Gruntfile.js
+++ Gruntfile.js
@@ -94,6 +94,6 @@ module.exports = function(grunt) {
   grunt.registerTask('serve', ['build', 'connect:dist:keepalive']);
 
   // Default task.
-  grunt.registerTask('default', ['jshint', 'qunit', 'concat', 'uglify']);
+  grunt.registerTask('default', 'serve');
 
 };
